59be12de816539a4834febe0465630c9db44364b
KM <you@example.com>
2015-03-16 13:46:21 +0200

    ma

--- toinen/app.js
+++ toinen/app.js
@@ -24,7 +24,9 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/newRecipe', routes.newRecipe);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- toinen/routes/index.js
+++ toinen/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.newRecipe = function(req,res){
+    res.render('newRecipe',{});
+});
+
 module.exports = router;
